5db65e951c96bda8cefa050a4daf4c715b954f4b
Juan-Pivo10 <juan.goes@estudante.ifms.edu.br>
2025-09-17 12:52:16 -0300

    criação function handleform

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { error } from 'console'
 import './App.css'
 //useeffect
 import { useEffect, useState } from 'react'
@@ -16,9 +17,34 @@ function App() {
       .then((response) => response.json())
       .then((data) => setProdutos(data))
   }, [])
+  function handleForm(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const form = event.currentTarget
+    const formData = new FormData(form)
+    
+    fetch('/api/produtos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then((response) => response.json())
+      .then((newProduto) => setProdutos([...produtos, newProduto]))
+      .catch((error) => console.error('Error posting data:', error))
+    form.reset()
+  }
   return ( 
   <>
-  <div>JUAN</div>
+  <div>Cadastro de Produtos</div>
+  <form onSubmit={handleForm}>
+    <input type="text" name="nome" placeholder="Nome" />
+    <input type="number" name="preco" placeholder="Preço" />
+    <input type="text" name="urlfoto" placeholder="URL da Foto" />
+    <input type="text" name="descricao" placeholder="Descrição" />
+    <button type="submit">Cadastrar</button>
+  </form>
+  <div>Lista de Produtos</div>
   {
     produtos.map((produto) => (
       <div key={produto.id}>
